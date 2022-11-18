8fb6f9b29adcc2219b8dbbc2061a1634e5de974b
Dhiraj-JC <dhiraj@trainitsolutions.com>
2022-11-18 20:46:10 +0530

    refact: baseURL changed

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000/";
+const baseUrl = "https://myspace22222222.herokuapp.com/";
 
 export function customGET(url) {
     const token = localStorage.getItem('token');
